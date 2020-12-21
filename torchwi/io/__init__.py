56e1ba260a6fa6e7d3de8b0f8de6903d01dc0a59
Wansoo Ha <wansooha@gmail.com>
2020-12-21 00:44:58 -0500

    load dataloaders in __init__

--- torchwi/io/__init__.py
+++ torchwi/io/__init__.py
@@ -1,4 +1,5 @@
 from .logger import MainLogger, JobLogger
 from .parser import CFGParser
-from .dataloader import FreqDataset, TimeDataset, TimeForwardDataset
-from .dataloader import time_forward_distributed_dataloader, time_distributed_dataloader
+from .FreqDataloader import FreqDataset
+from .TimeDataloader import TimeDataset, TimeForwardDataset
+from .TimeDataloader import time_forward_distributed_dataloader, time_distributed_dataloader
