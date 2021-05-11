a046a89703c4d34f53e2c752073c80975adb1e6f
Wansoo Ha <wansooha@gmail.com>
2021-05-11 00:34:08 -0400

    refactor time operators

---
+++ torchwi/operator/TimeFunction.py
@@ -0,0 +1,148 @@
+import torch
+
+def _get_propagator(device, prop='ext', exa=False, forward_only=False):
+    if device == 'cuda':
+        if forward_only:
+            from torchwi.propagator import td2d_forward_cuda as prop
+        elif exa:
+            from torchwi.propagator import td2d_exa_cuda as prop
+        else:
+            from torchwi.propagator import td2d_cuda as prop
+    else: # cpu
+        if prop == 'ext':
+            if forward_only:
+                from torchwi.propagator import td2d_forward_cpu as prop
+            elif exa:
+                from torchwi.propagator import td2d_exa_cpu as prop
+            else:
+                from torchwi.propagator import td2d_cpu as prop
+        elif prop == 'numba':
+            if forward_only:
+                raise NotImplementedError("numba forward_only")
+            elif exa:
+                raise NotImplementedError("numba exa")
+            else:
+                from torchwi.propagator import td2d_numba as prop
+        else:
+            raise NotImplementedError("Check prop=[ext|numba]")
+    return prop
+
+
+def get_operator(device, prop='ext', exa=False, forward_only=False):
+    _propagator = _get_propagator(device, prop, exa, forward_only)
+
+    if forward_only:
+
+        class TimeForwardOperator(torch.autograd.Function):
+
+            @staticmethod
+            def forward(ctx, vel, args):
+                # batch size=1
+                # input/output vel,grad: (nx,ny)
+                # frd: (nt,nx)
+                # virt: (nt,dimxy)
+                # internal vpad: (dimy,dimx)
+                sx,sy,ry,m = args
+
+                _propagator.forward(m.frd, 
+                        m.u1, m.u2, m.u3, m.vpad, m.w,
+                        m.order, m.dimx, m.dimy, m.nt,
+                        m.h, m.dt,
+                        sx.item(),sy.item(),ry.item())
+                        
+                return m.frd.view(m.nt,m.nx)[:,:m.nx_org]
+
+            @staticmethod
+            def backward(ctx, grad_output):
+                return None, None
+
+        return TimeForwardOperator.apply
+
+    elif exa:
+
+        class TimeEXAOperator(torch.autograd.Function):
+
+            @staticmethod
+            def forward(ctx, vel, args):
+                # batch size=1
+                # input/output vel,grad: (nx,ny)
+                # frd: (nt,nx)
+                # virt: (nt,dimxy)
+                # internal vpad: (dimy,dimx)
+                sx,sy,ry,m = args
+
+                _propagator.forward(m.frd, m.exa, m.iexa,
+                        m.u1, m.u2, m.u3, m.vpad, m.w,
+                        m.order, m.dimx, m.dimy, m.nt,
+                        m.h, m.dt,
+                        sx.item(),sy.item(),ry.item())
+                        
+                # save for gradient calculation
+                ctx.model = m
+                ctx.save_for_backward(m.exa, m.iexa, ry)
+                return m.frd.view(m.nt,m.nx)[:,:m.nx_org]
+
+            @staticmethod
+            def backward(ctx, grad_output):
+                # resid = grad_output: (nt,nx)
+                # grad_input: (nx,ny)
+                exa, iexa, ry = ctx.saved_tensors
+                m = ctx.model
+
+                resid = grad_output
+                grad = _propagator.backward(exa, iexa,
+                        m.u1, m.u2, m.u3, m.vpad, resid,
+                        m.order, m.dimx, m.dimy, m.nt,
+                        m.h, m.dt, ry.item())
+                # grad shape=(dimx,) # x fast
+                grad = grad.view(m.dimy,m.dimx)[m.ne:m.ne+m.ny_org,m.ne:m.ne+m.nx_org]
+                # grad_input: (nx,ny)
+                grad_input = grad.transpose(0,1)
+                return grad_input, None
+
+        return TimeEXAOperator.apply
+
+    else:
+
+        class TimeOperator(torch.autograd.Function):
+
+            @staticmethod
+            def forward(ctx, vel, args):
+                # batch size=1
+                # input/output vel,grad: (nx,ny)
+                # frd: (nt,nx)
+                # virt: (nt,dimxy)
+                # internal vpad: (dimy,dimx)
+                sx,sy,ry,m = args
+
+                _propagator.forward(m.frd, m.virt,
+                        m.u1, m.u2, m.u3, m.vpad, m.w,
+                        m.order, m.dimx, m.dimy, m.nt,
+                        m.h, m.dt,
+                        sx.item(),sy.item(),ry.item())
+                        
+                # save for gradient calculation
+                ctx.model = m
+                ctx.save_for_backward(m.virt,ry)
+                return m.frd.view(m.nt,m.nx)[:,:m.nx_org]
+
+            @staticmethod
+            def backward(ctx, grad_output):
+                # resid = grad_output: (nt,nx)
+                # grad_input: (nx,ny)
+                virt,ry = ctx.saved_tensors
+                m = ctx.model
+
+                resid = grad_output
+                grad = _propagator.backward(virt,
+                        m.u1, m.u2, m.u3, m.vpad, resid,
+                        m.order, m.dimx, m.dimy, m.nt,
+                        m.h, m.dt, ry.item())
+                # grad shape=(dimx,) # x fast
+                grad = grad.view(m.dimy,m.dimx)[m.ne:m.ne+m.ny_org,m.ne:m.ne+m.nx_org]
+                # grad_input: (nx,ny)
+                grad_input = grad.transpose(0,1)
+                return grad_input, None
+
+        return TimeOperator.apply
+
--- torchwi/operator/TimeOperator.py
+++ torchwi/operator/TimeOperator.py
@@ -1,196 +1,9 @@
 import torch
-import numpy as np
+from . import TimeFunction
 
 # for cuda shared memory: BDIMX, BDIMY same to those defined in "td2d_cuda.h"
 BDIMX = 16
 BDIMY = 16
-
-def get_operator(_propagator):
-
-    class TimeOperator(torch.autograd.Function):
-
-        @staticmethod
-        def forward(ctx, vel, args):
-            # batch size=1
-            # input/output vel,grad: (nx,ny)
-            # frd: (nt,nx)
-            # virt: (nt,dimxy)
-            # internal vpad: (dimy,dimx)
-            sx,sy,ry,m = args
-
-            _propagator.forward(m.frd, m.virt,
-                    m.u1, m.u2, m.u3, m.vpad, m.w,
-                    m.order, m.dimx, m.dimy, m.nt,
-                    m.h, m.dt,
-                    sx.item(),sy.item(),ry.item())
-                    
-            # save for gradient calculation
-            ctx.model = m
-            ctx.save_for_backward(m.virt,ry)
-            return m.frd.view(m.nt,m.nx)[:,:m.nx_org]
-
-        @staticmethod
-        def backward(ctx, grad_output):
-            # resid = grad_output: (nt,nx)
-            # grad_input: (nx,ny)
-            virt,ry = ctx.saved_tensors
-            m = ctx.model
-
-            resid = grad_output
-            grad = _propagator.backward(virt,
-                    m.u1, m.u2, m.u3, m.vpad, resid,
-                    m.order, m.dimx, m.dimy, m.nt,
-                    m.h, m.dt, ry.item())
-            # grad shape=(dimx,) # x fast
-            grad = grad.view(m.dimy,m.dimx)[m.ne:m.ne+m.ny_org,m.ne:m.ne+m.nx_org]
-            # grad_input: (nx,ny)
-            grad_input = grad.transpose(0,1)
-            return grad_input, None
-
-    return TimeOperator.apply
-
-
-def get_exa_operator(_propagator):
-
-    class TimeOperator(torch.autograd.Function):
-
-        @staticmethod
-        def forward(ctx, vel, args):
-            # batch size=1
-            # input/output vel,grad: (nx,ny)
-            # frd: (nt,nx)
-            # virt: (nt,dimxy)
-            # internal vpad: (dimy,dimx)
-            sx,sy,ry,m = args
-
-            _propagator.forward(m.frd, m.exa, m.iexa,
-                    m.u1, m.u2, m.u3, m.vpad, m.w,
-                    m.order, m.dimx, m.dimy, m.nt,
-                    m.h, m.dt,
-                    sx.item(),sy.item(),ry.item())
-                    
-            # save for gradient calculation
-            ctx.model = m
-            ctx.save_for_backward(m.exa, m.iexa, ry)
-            return m.frd.view(m.nt,m.nx)[:,:m.nx_org]
-
-        @staticmethod
-        def backward(ctx, grad_output):
-            # resid = grad_output: (nt,nx)
-            # grad_input: (nx,ny)
-            exa, iexa, ry = ctx.saved_tensors
-            m = ctx.model
-
-            resid = grad_output
-            grad = _propagator.backward(exa, iexa,
-                    m.u1, m.u2, m.u3, m.vpad, resid,
-                    m.order, m.dimx, m.dimy, m.nt,
-                    m.h, m.dt, ry.item())
-            # grad shape=(dimx,) # x fast
-            grad = grad.view(m.dimy,m.dimx)[m.ne:m.ne+m.ny_org,m.ne:m.ne+m.nx_org]
-            # grad_input: (nx,ny)
-            grad_input = grad.transpose(0,1)
-            return grad_input, None
-
-    return TimeOperator.apply
-
-
-def get_forward_operator(_propagator):
-
-    class TimeOperator(torch.autograd.Function):
-
-        @staticmethod
-        def forward(ctx, vel, args):
-            # batch size=1
-            # input/output vel,grad: (nx,ny)
-            # frd: (nt,nx)
-            # virt: (nt,dimxy)
-            # internal vpad: (dimy,dimx)
-            sx,sy,ry,m = args
-
-            _propagator.forward(m.frd, 
-                    m.u1, m.u2, m.u3, m.vpad, m.w,
-                    m.order, m.dimx, m.dimy, m.nt,
-                    m.h, m.dt,
-                    sx.item(),sy.item(),ry.item())
-                    
-            return m.frd.view(m.nt,m.nx)[:,:m.nx_org]
-
-        @staticmethod
-        def backward(ctx, grad_output):
-            return None, None
-
-    return TimeOperator.apply
-
-
-class Time2d(torch.nn.Module):
-    def __init__(self,nx,ny,h,w,dt,order,device, prop='ext',exa=False):
-        """
-        input/output vel,grad shape = (nx,ny) # y fast
-        interval vpad shape = (dimy,dimx) # x fast
-        """
-        super(Time2d, self).__init__()
-        self._exa = exa
-
-        self.device = device
-        self.h = h
-        self.w = w.to('cpu') # wavelet in cpu for both cpu/cuda
-        self.nt = len(w)
-        self.dt = dt
-
-        self.order = order
-        self.ne = order//2
-        self.nx_org, self.ny_org = nx,ny
-
-        if self.device == 'cuda':
-            self.nx=dim_pad(self.nx_org,BDIMX)
-            self.ny=dim_pad(self.ny_org,BDIMY)
-            if self._exa:
-                from torchwi.propagator import td2d_exa_cuda
-                self.time_modeling = get_exa_operator(td2d_exa_cuda)
-            else:
-                from torchwi.propagator import td2d_cuda
-                self.time_modeling = get_operator(td2d_cuda)
-        elif prop=='ext':
-            self.nx = self.nx_org
-            self.ny = self.ny_org
-            if self._exa:
-                from torchwi.propagator import td2d_exa_cpu
-                self.time_modeling = get_exa_operator(td2d_exa_cpu)
-            else:
-                from torchwi.propagator import td2d_cpu
-                self.time_modeling = get_operator(td2d_cpu)
-        elif prop=='numba':
-            self.nx = self.nx_org
-            self.ny = self.ny_org
-            from torchwi.propagator import TimeProp
-            self.time_modeling = get_operator(TimeProp)
-
-        self.dimx = self.nx + self.order
-        self.dimy = self.ny + self.order
-        self.dimxy = self.dimx * self.dimy
-
-        # work array for efficiency
-        self.u1   = torch.zeros(self.dimxy, device=self.device)
-        self.u2   = torch.zeros(self.dimxy, device=self.device)
-        self.u3   = torch.zeros(self.dimxy, device=self.device)
-        self.frd  = torch.zeros((self.nt*self.nx), device=self.device)
-        if self._exa:
-            self.exa = torch.zeros(self.dimxy, device=self.device)
-            self.iexa = torch.zeros(self.dimxy, dtype=torch.int16, device=self.device)
-        else:
-            self.virt = torch.zeros((self.nt*self.dimxy), device=self.device)
-
-    def pad_vel(self,vel):
-        pad_shape = (self.ne, self.nx-self.nx_org+self.ne, self.ne, self.ny-self.ny_org+self.ne)
-        with torch.no_grad():
-            self.vpad = torch.nn.ReplicationPad2d(pad_shape)(vel.transpose(0,1).view(1,1,self.ny_org,self.nx_org)).view(-1)
-
-    def forward(self, vel, sx,sy,ry):
-        # vel (nx,ny)
-        # batchsize=1 to save memory for source wavefield
-        self.pad_vel(vel)
-        return self.time_modeling(vel, (sx,sy,ry,self))
 
 
 # for gpu shared memory
@@ -223,16 +36,15 @@ class Time2dForward(torch.nn.Module):
         if self.device == 'cuda':
             self.nx=dim_pad(self.nx_org,BDIMX)
             self.ny=dim_pad(self.ny_org,BDIMY)
-            from torchwi.propagator import td2d_forward_cuda
-            self.time_modeling = get_forward_operator(td2d_forward_cuda)
         else:
             self.nx = self.nx_org
             self.ny = self.ny_org
-            from torchwi.propagator import td2d_forward_cpu
-            self.time_modeling = get_forward_operator(td2d_forward_cpu)
+
         self.dimx = self.nx + self.order
         self.dimy = self.ny + self.order
         self.dimxy = self.dimx * self.dimy
+
+        self.time_modeling = TimeFunction.get_operator(self.device, forward_only=True)
 
         # work array for efficiency
         self.u1   = torch.zeros(self.dimxy, device=self.device)
@@ -250,3 +62,22 @@ class Time2dForward(torch.nn.Module):
         # batchsize=1 to save memory for source wavefield
         self.pad_vel(vel)
         return self.time_modeling(vel, (sx,sy,ry,self))
+
+
+
+class Time2d(Time2dForward):
+    def __init__(self,nx,ny,h,w,dt,order,device, prop='ext',exa=False):
+        """
+        input/output vel,grad shape = (nx,ny) # y fast
+        interval vpad shape = (dimy,dimx) # x fast
+        """
+        super(Time2d, self).__init__(nx,ny,h,w,dt,order,device)
+        self.time_modeling = TimeFunction.get_operator(self.device, prop, exa)
+
+        # work array for efficiency
+        if exa:
+            self.exa = torch.zeros(self.dimxy, device=self.device)
+            self.iexa = torch.zeros(self.dimxy, dtype=torch.int16, device=self.device)
+        else:
+            self.virt = torch.zeros((self.nt*self.dimxy), device=self.device)
+
