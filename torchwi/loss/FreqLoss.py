b1c1a38699f0995007bf1c15cd16849d294b77ea
Wansoo Ha <wansooha@gmail.com>
2022-08-23 15:26:03 +0900

    use torch complex tensor

--- torchwi/loss/FreqLoss.py
+++ torchwi/loss/FreqLoss.py
@@ -1,23 +1,16 @@
 import torch
-import numpy as np
-from torchwi.utils.ctensor import ca2rt, rt2ca
 
 
 class FreqL2Loss(torch.autograd.Function):
     @staticmethod
     def forward(ctx, frd, true):
-        # resid: (nrhs, 2*nx) 2 for real and imaginary
-        resid = frd - true
-        resid_c = rt2ca(resid)
-        l2 = np.real(0.5*np.sum(resid_c*np.conjugate(resid_c)))
+        resid = frd - true # complex value
+        l2 = torch.real(0.5*torch.sum(resid*torch.conj(resid)))
         ctx.save_for_backward(resid)
-        return torch.tensor(l2)
+        return l2
 
     @staticmethod
     def backward(ctx, grad_output):
         resid, = ctx.saved_tensors
-        grad_input = ca2rt(np.conjugate(rt2ca(resid)))
+        grad_input = torch.conj(resid)
         return grad_input, None
-
-
-
